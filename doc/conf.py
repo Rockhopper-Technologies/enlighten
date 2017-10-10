9df5744bd26d730272a7dec83e454f0233d4dff6
Avram Lubkin <aviso@rockhopper.net>
2017-10-10 12:49:06 -0400

    Don't require sphinxcontrib.spelling on readthedocs.org

--- doc/conf.py
+++ doc/conf.py
@@ -32,10 +32,12 @@ from setup_helpers import get_version
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinxcontrib.spelling',
-              'sphinx.ext.autodoc',
+extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.intersphinx',
               'sphinx.ext.napoleon']
+
+if not os.environ.get('READTHEDOCS') == 'True':
+    extensions.append('sphinxcontrib.spelling')
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
