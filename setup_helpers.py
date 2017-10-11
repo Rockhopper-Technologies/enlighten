cfc548ba77e352e305ca68c63e8cb7807faddc10
Avram Lubkin <aviso@rockhopper.net>
2017-10-11 18:57:31 -0400

    Improve doc spelling test

--- setup_helpers.py
+++ setup_helpers.py
@@ -8,7 +8,9 @@
 Functions to help with build and setup
 """
 
+import os
 import re
+import sys
 
 
 RE_VERSION = re.compile(r'__version__\s*=\s*[\'\"](.+)[\'\"]$')
@@ -26,3 +28,38 @@ def get_version(filename):
                 return version.group(1)
 
     return None
+
+
+def print_spelling_errors(filename):
+    """
+    Print misspelled words returned by sphinxcontrib-spelling
+    """
+
+    filesize = os.stat(filename).st_size
+    if filesize:
+        sys.stdout.write('Misspelled Words:\n')
+        with open(filename) as wordlist:
+            for line in wordlist:
+                sys.stdout.write('    ' + line)
+        return 1
+    else:
+        return 0
+
+
+if __name__ == '__main__':
+
+    # Do nothing if no arguments were given
+    if len(sys.argv) < 2:
+        sys.exit(0)
+
+    # Print misspelled word list
+    if sys.argv[1] == 'spelling':
+        if len(sys.argv) > 2:
+            sys.exit(print_spelling_errors(sys.argv[2]))
+        else:
+            sys.exit(print_spelling_errors('build/doc/spelling/output.txt'))
+
+    # Unknown option
+    else:
+        sys.stderr.write('Unknown option: %s' % sys.argv[1])
+        sys.exit(1)
