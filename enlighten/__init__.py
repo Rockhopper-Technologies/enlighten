a4045bbe0e52c996d670c74a9cc077fcd76da2dd
Avram Lubkin <avylove@rockhopper.net>
2020-05-23 10:04:36 -0700

    Bump version to 1.5.2

--- enlighten/__init__.py
+++ enlighten/__init__.py
@@ -15,5 +15,5 @@ from enlighten.counter import Counter, SubCounter
 from enlighten._manager import Manager, get_manager
 
 
-__version__ = '1.5.1'
+__version__ = '1.5.2'
 __all__ = ('Counter', 'Manager', 'SubCounter', 'get_manager')
