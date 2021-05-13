c018f23f280ed996751b5d3f57fa289fbf5b739b
Avram Lubkin <avylove@rockhopper.net>
2021-05-13 08:56:44 -0400

    Update docs copyright

--- doc/conf.py
+++ doc/conf.py
@@ -53,7 +53,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Enlighten'
-copyright = '2017 - 2020, Avram Lubkin'
+copyright = '2017 - 2021, Avram Lubkin'
 author = 'Avram Lubkin'
 
 # The version info for the project you're documenting, acts as replacement for
